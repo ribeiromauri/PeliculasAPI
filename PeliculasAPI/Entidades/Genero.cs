4bd98af594d625895e458bfc7fc0d66559b38dc8
ribeiromauri <110779735+ribeiromauri@users.noreply.github.com>
2023-07-22 17:42:31 -0300

    Agregar archivos de proyecto.

---
+++ PeliculasAPI/Entidades/Genero.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasAPI.Entidades
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Nombre { get; set; }
+    }
+}
